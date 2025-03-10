c5217645c656579d6af8ea95c25bff0f1738bfb0
EMERSON <emersonleist@yahoo.ca>
2025-03-09 21:58:21 -0700

    last part of day 8 follow along

--- patterns/book-review.php
+++ patterns/book-review.php
@@ -8,8 +8,8 @@
 <!-- Pattern code goes here. -->
 
 <!-- wp:media-text {"mediaId":34,"mediaLink":"http://localhost/mindset/women-office/","mediaType":"image","mediaWidth":40,"mediaSizeSlug":"large"} -->
-<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="http://localhost/mindset/wp-content/uploads/2025/03/women-office-1024x682.jpg" alt="person working in chair" class="wp-image-34 size-large"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
-<h2 class="wp-block-heading">Title</h2>
+<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="<?php get_theme_file_uri( 'assets/images/book-cover.jpg' ) ?>" alt="<?php esc_html_e( 'placeholder image of a book', 'mindset-theme' ) ?>" class="wp-image-34 size-large"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e( 'Title', 'mindset-theme' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
