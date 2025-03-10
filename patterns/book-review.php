8801cee6e437775d72bf22ef4b49334872d69632
EMERSON <emersonleist@yahoo.ca>
2025-03-09 19:52:46 -0700

    minor fixes

--- patterns/book-review.php
+++ patterns/book-review.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Title: book review
+ * Slug: mindset-theme/book-review
+ * Categories: media, text
+ */
+?>
+<!-- Pattern code goes here. -->
+
 <!-- wp:media-text {"mediaId":34,"mediaLink":"http://localhost/mindset/women-office/","mediaType":"image","mediaWidth":40,"mediaSizeSlug":"large"} -->
 <div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="http://localhost/mindset/wp-content/uploads/2025/03/women-office-1024x682.jpg" alt="person working in chair" class="wp-image-34 size-large"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
 <h2 class="wp-block-heading">Title</h2>
